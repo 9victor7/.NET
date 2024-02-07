c911647ab6b2e1950ddebde625fe2ba5f698b617
9victor7 <9victor.arias@gmail.com>
2024-02-07 15:34:07 -0500

    avance proyecto ASP.NET

---
+++ CursoWebMVC/CapaEntidad/DetalleVenta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class DetalleVenta
+    {
+//        CREATE TABLE DETALLE_VENTA(
+//IdDetalleVenta int primary key identity,
+//IdVenta int references VENTA(IdVenta),
+//IdProducto int references PRODUCTO(IdProducto),
+//Cantidad int,
+//Total decimal(10,2),
+
+        public int IdDetalleVenta { get; set; }
+        public int IdVenta { get; set;}
+        public Producto oProducto { get; set;}
+        public int Cantidad { get; set; }
+        public decimal Total { get; set; }
+        public string IdTransaccion { get; set; }
+
+        public List<DetalleVenta> oDetalleVenta { get; set; }
+    }
+}
